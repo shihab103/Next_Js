29bd6c2b5da3249490f24a5af5e3413cc8377b3c
Shihab <shihabuddin2469@gmail.com>
2025-09-18 12:04:55 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +19,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <NavBar/>
+        <main className="h-screen h-max-[600px]">{children}</main>
       </body>
     </html>
   );
---
+++ src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServicesPage = () => {
+    return (
+        <div>
+            <p className='font-bold text-2xl'>services page</p>
+        </div>
+    );
+};
+
+export default ServicesPage;
